4e7fe0b187dd862ba93f15562abd3546779fd198
Andres Perez <andres@tresata.com>
2013-07-08 11:22:09 -0400

    Added solution for problem 32: pandigital products.

---
+++ 032.scala
@@ -0,0 +1,24 @@
+/*
+We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once;
+for example, the 5-digit number, 15234, is 1 through 5 pandigital.
+
+The product 7254 is unusual, as the identity, 39 x 186 = 7254, containing multiplicand, multiplier,
+and product is 1 through 9 pandigital.
+
+Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital.
+
+HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
+*/
+
+val digits = (1 to 9).toSet
+def isPandigital(n: Int): Boolean = {
+  val nDigits = n.toString.toIndexedSeq[Char].map(d => d.asDigit)
+  val otherDigits = (digits -- nDigits).toIndexedSeq
+  lazy val hasProduct = (1 to otherDigits.size / 2)
+    .map(otherDigits.combinations(_)).flatten
+    .map(_.permutations).flatten
+    .map(m1 => (otherDigits diff m1).permutations.map((m1, _)).toList).flatten
+    .exists(m1m2 => (m1m2._1.mkString.toInt * m1m2._2.mkString.toInt == n))
+  !nDigits.contains(0) && nDigits == nDigits.distinct && hasProduct
+}
+val A32 = (1234 to 9876).filter(isPandigital(_)).sum
