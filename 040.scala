dc53ded4093c1f8d44fd5912e4c9636397ef6b4f
Andres Perez <andy327@gmail.com>
2013-07-12 00:09:12 -0500

    Elegant solution to problem 40: Champernowne's constant.

---
+++ 040.scala
@@ -0,0 +1,14 @@
+/*
+An irrational decimal fraction is created by concatenating the positive integers:
+
+0.123456789101112131415161718192021...
+
+It can be seen that the 12th digit of the fractional part is 1.
+
+If d_n represents the nth digit of the fractional part, find the value of the following expression.
+
+d_1 x d_10 x d_100 x d_1000 x d_10000 x d_100000 x d_1000000
+*/
+
+val champernowne = Stream.iterate(0)(_ + 1).flatMap{ n => n.toString.map(_.asDigit) }
+val A40 = List.iterate(1, 7)(_ * 10).map(champernowne(_)).product
