e8185528ee793484cb4b06ca43afa5d913012b39
Andres Perez <andres@tresata.com>
2013-07-31 19:15:22 -0400

    Dummy commit: unsaved changes to problem 62.

--- 051-075/062.scala
+++ 051-075/062.scala
@@ -5,31 +5,13 @@ In fact, 41063625 is the smallest cube which has exactly three permutations of i
 Find the smallest cube for which exactly five permutations of its digits are cube.
 */
 
-def isCube(n: Long): Boolean = scala.math.cbrt(n) % 1 == 0
 val cubes = Stream.iterate(0L)(_ + 1).map(scala.math.pow(_, 3).toLong)
-def arePermutations(n1: Long, n2: Long): Boolean = n1.toString.sorted == n2.toString.sorted
-def countCubePermutations(n: Long): Int = {
-  val nCubed = scala.math.pow(n, 3).toLong
-  cubes.takeWhile(_ <= nCubed * 10).filter(arePermutations(_, nCubed)).size
-}
-val A62 = Stream.iterate(1L)(_ + 1).find(countCubePermutations(_) == 5).map(scala.math.pow(_, 3).toLong).get
-
-
-val cubes = Stream.iterate(0L)(_ + 1).map(scala.math.pow(_, 3).toLong)
-def isCube(n: Long): Boolean = scala.math.cbrt(n) % 1 == 0
-def arePermutations(n1: Long, n2: Long): Boolean = n1.toString.sorted == n2.toString.sorted
-def countCubePermutations(n: Long): Int = {
-  val nCubed = scala.math.pow(n, 3).toLong
-  cubes.takeWhile(_ <= nCubed * 10).filter(arePermutations(_, nCubed)).size
-}
-def lowestCubePermutation(n: Long): Long = {
-  val nCubed = scala.math.pow(n, 3).toLong
-  cubes.takeWhile(_ <= nCubed * 10).filter(arePermutations(_, nCubed)).min
-}
 val cubeMap = collection.mutable.Map[String, Int]().withDefaultValue(0)
-val highRoot = cubes
-  .map(cb => { cubeMap(cb.toString.sorted) += 1; cb.toString.sorted })
-  .zipWithIndex
-  .find{ case (n, i) => cubeMap(n) == 5 && countCubePermutations(i) == 5 }
-  .get._2
-val A62 = lowestCubePermutation(highRoot.toLong)
+def key(n: Long) = n.toString.sorted
+def cubePermutations(n: Int): Seq[Long] = {
+  val nCubed = scala.math.pow(n, 3).toLong
+  cubes.takeWhile(_ <= nCubed * 10).filter(key(_) == key(nCubed)).toSeq
+}
+val cubeKeys = cubes.map(cb => { cubeMap(key(cb)) += 1; key(cb) })
+val highRoot = cubeKeys.zipWithIndex.find{ case (cb, n) => cubeMap(cb) >= 5 && cubePermutations(n).size == 5 }.get._2.toInt
+val A62 = cubePermutations(highRoot).min
